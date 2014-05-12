e66230ca987ce5c93ab239f3c475b6f8e2926f90
Andrew Reynolds <andrew@opentechinstitute.org>
2014-05-12 11:37:58 -0400

    Added example comments

--- commotiontestobjects/commotionrouterobjects/pageobjects/crpageobjects.py
+++ commotiontestobjects/commotionrouterobjects/pageobjects/crpageobjects.py
@@ -2,14 +2,32 @@
     Individual page elements are contained in xelements.py files.
 """
 
+import commotiontestobjects.browserobjects
+
 class CRCommonPageObjects(object):
     """Page objects common to all Commotion Router pages"""
-
-    thisnode = None
     
+    # Example
+    # From http://justinlilly.com/python/selenium-page-object-pattern-\
+    #    -the-key-to-maintainable-tests.html
+    # These are currently handled in tests and browserobjects
+    #url = None
+
+    #def __init__(self, driver):
+        #self.driver = driver
+
+    #def fill_form_by_css(self, form_css, value):
+        #elem = self.driver.find(form_css)
+        #elem.send_keys(value)
+
+    #def fill_form_by_id(self, form_element_id, value):
+        #return self.fill_form_by_css('#%s' % form_element_id, value)
+
+    #def navigate(self):
+        #self.driver.get(self.url)
+    # End example
 
     # thisnode url
-    thisnode = self.driver.netinfo.commotion_node_ip
     # Header
     # Footer
     # Body
