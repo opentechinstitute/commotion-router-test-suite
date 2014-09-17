19ca910d23d15cfc093d071c909795882c571f09
Andrew Reynolds <andrew@opentechinstitute.org>
2014-09-17 11:21:33 -0500

    update CommotionIPError exception

--- objects/exceptions.py
+++ objects/exceptions.py
@@ -1,14 +1,14 @@
 """Custom exceptions for the Commotion Router Test Suite"""
 
-import requests.exceptions.ConnectionError
+import requests
 
-class CommotionIPError(ConnectionError)
+class CommotionIPError(ConnectionError):
     """
     Raised when the test host does not have a valid Commotion IP address.
     Subclass of the built-in ConnectionError base class.
     """
 
-    def __init__(self):
+    def __init__(self, arg):
         self.msg = (
             "No valid Commotion IP address found on any network interface"
             )
