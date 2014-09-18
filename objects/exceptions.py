cf8d44048b97c3437a79bb745f0e8ed2739badd9
Andrew Reynolds <andrew@opentechinstitute.org>
2014-09-18 09:17:51 -0500

    Give CommotionIPError default message and arguments

--- objects/exceptions.py
+++ objects/exceptions.py
@@ -7,8 +7,9 @@ class CommotionIPError(ConnectionError):
     Raised when the test host does not have a valid Commotion IP address.
     Subclass of the built-in ConnectionError base class.
     """
-
-    def __init__(self, arg):
-        self.msg = (
-            "No valid Commotion IP address found on any network interface"
-            )
+    def __init__(self, value):
+        self.msg = "No valid Commotion IP address found: "
+        self.value = value
+    def __str__(self):
+        string = self.msg + repr(self.value)
+        return string
