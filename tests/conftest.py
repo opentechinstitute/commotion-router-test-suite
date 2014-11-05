f1c006d3f7866e1d4a7e2bc02d6343b880eaad65
Andrew Reynolds <andrew@opentechinstitute.org>
2014-11-05 13:48:17 -0600

    Clean test code out of conftest.py
    Remove unused packaging file (setup.py)

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,26 +4,14 @@ Config instructions and test fixtures
 
 import pytest
 import os
-import sys
-
-
-# # these are just some fun dividiers to make the output pretty
-# # completely unnecessary, I was just playing with autouse fixtures
-# @pytest.fixture(scope="function", autouse=True)
-# def divider_function(request):
-#     print('\n        --- function %s() start ---' % request.function.__name__)
-#     def fin():
-#             print('        --- function %s() done ---' % request.function.__name__)
-#     request.addfinalizer(fin)
 
 
 @pytest.fixture(scope="session", autouse=True)
-def set_up_ini(request):
+def setup_ini(request):
     print("in set_up_ini")
     try:
         # need to back up a directory
         path = os.path.dirname(os.path.abspath("conftest.py"))
-        print(path)
         if not os.path.isfile(path + "/pytest.ini"):
             raise FileNotFoundError("Pytest.ini not found.")
     except FileNotFoundError as args:
@@ -34,5 +22,8 @@ def set_up_ini(request):
             shutil.copyfile(path + "/example-pytest.ini", path + "/pytest.ini")
         except OSError as args:
             print("Error creating pytest.ini. ", args)
-    else:
-        print("Don't forget to add node admin credentials to pytest.ini!")
+
+    # # Necessary?
+    # def finalize_setup_ini():
+    #     return None
+    #     request.addfinalizer(finalize_setup_ini)
